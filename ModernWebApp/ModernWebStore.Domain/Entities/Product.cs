3a425728588b93bf6bff2028ec149efe793e86e3
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-10-27 12:35:46 -0200

    Parte 1 BackEnd

---
+++ ModernWebApp/ModernWebStore.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWebStore.Domain.Entities
+{
+    public class Product
+    {
+        public Product(string title, string description, decimal price, int quantityOnHand, int category)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Price = price;
+            this.QuantityOnHand = quantityOnHand;
+            this.CategoryId = category;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int QuantityOnHand { get; private set; }
+        public int CategoryId { get; private set; } //Por causa do Entity Framework só de colocar o nome da classe e a propriedade ID ele consegue relacionar os objetos
+        public Category Category { get; private set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Projeto Modern Web Apps
